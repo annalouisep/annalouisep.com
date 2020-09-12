f228171450345ccc4af94ef7d04cd9c6558a2107
Anna Louise Percy <annalouisepercy@gmail.com>
2020-09-11 21:18:39 -0500

    alp-09-11-2020 add bash deploy script

---
+++ deploy.sh
@@ -0,0 +1,5 @@
+git clone https://github.com/annalouisep/annalouisep.com.git
+mv annalouisep.com/index.html /srv/www/annalouisep.com/
+mv annalouisep.com/functions.js /srv/www/annalouisep.com/
+mv annalouisep.com/style.css /srv/www/annalouisep.com
+rm -rf annalouisep.com/
