49e7429e49c9d1ecfedb7a06ee7d878c4ad04cc1
Anna Louise Percy <annalouisepercy@gmail.com>
2021-06-30 21:08:52 -0500

    update deploy script to add headshot

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,5 @@ git clone https://github.com/annalouisep/annalouisep.com.git
 mv annalouisep.com/index.html /srv/www/annalouisep.com/
 mv annalouisep.com/functions.js /srv/www/annalouisep.com/
 mv annalouisep.com/style.css /srv/www/annalouisep.com
+mv annalouisep.com/anna-desktop.jpg /srv/www/annalouisep.com
 rm -rf annalouisep.com/
