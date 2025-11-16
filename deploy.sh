b69acbeb6af665e8936dc7655f53de5c3d3fe8cf
Anna Louise Percy <annalouisepercy@gmail.com>
2025-11-16 17:42:14 -0600

    adding pottery section, fixing alignment of headshot

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,5 @@ mv annalouisep.com/index.html /srv/www/annalouisep.com/
 mv annalouisep.com/functions.js /srv/www/annalouisep.com/
 mv annalouisep.com/style.css /srv/www/annalouisep.com
 mv annalouisep.com/anna-desktop.jpg /srv/www/annalouisep.com
+mv annalouisep.com/pottery /srv/www/annalouisep.com
 rm -rf annalouisep.com/
